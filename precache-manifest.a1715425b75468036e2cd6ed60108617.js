52c2d03600cb6383dcff0d3032772d3b5c9d304e
vpidkivka <vpidkivka@eliteit.org>
2020-09-15 22:16:08 +0300

    Updates

---
+++ precache-manifest.a1715425b75468036e2cd6ed60108617.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd292b5bef53eda8482a01c9a3ae9e6c",
+    "url": "/i-decision-games/index.html"
+  },
+  {
+    "revision": "04d3d5e41ebd37aef4bf",
+    "url": "/i-decision-games/static/css/main.c267420c.chunk.css"
+  },
+  {
+    "revision": "2051ed812b3d9936e6e8",
+    "url": "/i-decision-games/static/js/2.c4a6d119.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/i-decision-games/static/js/2.c4a6d119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d3d5e41ebd37aef4bf",
+    "url": "/i-decision-games/static/js/main.0c2efd88.chunk.js"
+  },
+  {
+    "revision": "4fdd363165aa180db3d0",
+    "url": "/i-decision-games/static/js/runtime-main.38423f74.js"
+  },
+  {
+    "revision": "6329c0a65b34d8effdccd8bed9cd749d",
+    "url": "/i-decision-games/static/media/next-arrow.6329c0a6.svg"
+  },
+  {
+    "revision": "6962a1740eec94dc23305555450f1650",
+    "url": "/i-decision-games/static/media/prev-arrow.6962a174.svg"
+  }
+]);
